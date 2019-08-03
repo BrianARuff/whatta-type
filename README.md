f73a8c9e77f1a9a8ccbf78dd2514dad36204e9bf
Brian Ruff <thelonewolf198927@gmail.com>
2019-08-03 18:51:02 -0400

    type checking functions added

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # whatta-type
+---
+
+## **How to import package**
+```
+const types = require("WhattaType.js");
+```
+
+---
+
+### **How to use package**
+Here are some examples cases of how to use the package
+```
+console.log(types.isArray(["1", "2", "3"])); // true
+
+console.log(types.isArray("I am a string")); // false
+```
+---
+
+#### **TODO**
+1. Add testing with Jest
+---
+
+#### **Notes:**
+This package is the equivalent of the text information gathered [here](https://webbjocke.com/javascript-check-data-types/). I did not create the code in this package rather I found that this was the most accurate source for verifying types at runtime that I could find and put it in package format so that other's could use it in their programs if they wanted w/o having to look-it-up everytime. The code is still open source with an ISC license for everyone!
+
+---
---
+++ whatta-type..js
@@ -0,0 +1,62 @@
+const isString = (value) => {
+  return typeof value === 'string' || value instanceof String;
+}
+
+const isNumber = (value) => {
+  return typeof value === 'number' && isFinite(value);
+}
+
+const isArray = (value) => {
+  return value && typeof value === 'object' && value.constructor === Array;
+}
+
+const isFunction = (value) => {
+  return typeof value === 'function';
+}
+
+const isObject = (value) => {
+  return value && typeof value === 'object' && value.constructor === Object;
+}
+
+const isNull = (value) => {
+  return value === null;
+}
+
+const isUndefined = (value) => {
+  return typeof value === 'undefined';
+}
+
+const isBoolean = (value) => {
+  return typeof value === 'boolean';
+}
+
+const isRegExp = (value) => {
+  return value && typeof value === 'object' && value.constructor === RegExp;
+}
+
+const isError = (value) => {
+  return value instanceof Error && typeof value.message !== 'undefined';
+}
+
+const isDate = (value) => {
+  return value instanceof Date;
+}
+
+const isSymbol = (value) => {
+  return typeof value === 'symbol';
+}
+
+module.exports = {
+  isString,
+  isNumber,
+  isArray,
+  isFunction,
+  isObject,
+  isNull,
+  isUndefined,
+  isBoolean,
+  isRegExp,
+  isError,
+  isDate,
+  isSymbol,
+}
